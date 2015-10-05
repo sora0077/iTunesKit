28e9a7d2f69edd57cd7dce4024c46d8c93e21f34
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-10-06 01:56:10 +0900

    Add other search result type

--- iTunesKit/src/Endpoint/Search.swift
+++ iTunesKit/src/Endpoint/Search.swift
@@ -103,9 +103,8 @@ extension Search: iTunesRequestToken {
             let wrapperType = SearchResultWrapperType(rawValue: v["wrapperType"] as! String)!
             switch wrapperType {
             case .Track:
-                return SearchResultTrack(
-                    wrapperType: wrapperType,
-                    kind: v["kind"] as! String,
+                return .Track(SearchResultTrack(
+                    kind: SearchResultKind(rawValue: v["kind"] as! String)!,
                     artistId: v["artistId"] as! Int,
                     collectionId: v["collectionId"] as! Int,
                     trackId: v["trackId"] as! Int,
@@ -149,11 +148,54 @@ extension Search: iTunesRequestToken {
                     radioStationUrl: v["radioStationUrl"] as? String,
                     
                     isStreamable: v["isStreamable"] as! Bool
-                )
+                ))
             case .Artist:
-                fatalError("")
+                return .Artist(SearchResultArtist(
+                    artistType: v["artistType"] as! String,
+                    artistName: v["artistName"] as! String,
+                    artistLinkUrl: v["artistLinkUrl"] as! String,
+                    artistId: v["artistId"] as! Int,
+                    amgArtistId: v["amgArtistId"] as! Int,
+                    
+                    primaryGenreName: v["primaryGenreName"] as! String,
+                    primaryGenreId: v["primaryGenreId"] as! Int,
+                    
+                    radioStationUrl: v["radioStationUrl"] as? String
+                ))
             case .Collection:
-                fatalError("")
+                return .Collection(SearchResultCollection(
+                    collectionType: v["collectionType"] as! String,
+                    artistId: v["artistId"] as! Int,
+                    collectionId: v["collectionId"] as! Int,
+                    amgArtistId: v["amgArtistId"] as! Int,
+                    artistName: v["artistName"] as! String,
+                    collectionName: v["collectionName"] as! String,
+                    collectionCensoredName: v["collectionCensoredName"] as! String,
+                    
+                    artistViewUrl: v["artistViewUrl"] as! String,
+                    collectionViewUrl: v["collectionViewUrl"] as! String,
+                    
+                    artworkUrl30: v["artworkUrl30"] as! String,
+                    artworkUrl60: v["artworkUrl60"] as! String,
+                    artworkUrl100: v["artworkUrl100"] as! String,
+                    
+                    collectionPrice: v["collectionPrice"] as! Float,
+                    
+                    collectionExplicitness: v["collectionExplicitness"] as! String,
+                    
+                    trackCount: v["trackCount"] as! Int,
+                    
+                    copyright: v["copyright"] as! String,
+                    
+                    country: v["country"] as! String,
+                    currency: v["currency"] as! String,
+                    
+                    releaseDate: v["releaseDate"] as! String,
+                    
+                    primaryGenreName: v["primaryGenreName"] as! String,
+                    
+                    radioStationUrl: v["radioStationUrl"] as? String
+                ))
             }
         }
     }
--- iTunesKit/src/Entity/Search/SearchResult.swift
+++ iTunesKit/src/Entity/Search/SearchResult.swift
@@ -8,88 +8,132 @@
 
 import Foundation
 
-public enum SearchResultWrapperType: String {
+enum SearchResultWrapperType: String {
     case Track = "track"
     case Collection = "collection"
     case Artist = "artist"
-//    case Book = "book"
-//    case Album = "album"
-//    case CoachedAudio = "coached-audio"
-//    case FeatureMovie = "feature-movie"
-//    case InteractiveBooklet = "interactive-booklet"
-//    case MusicVideo = "music-video"
-//    case PdfPodcast = "pdf podcast"
-//    case PodcastEpisode = "podcast-episode"
-//    case SoftwarePackage = "software-package"
-//    case Song = "song"
-//    case TvEpisode = "tv-episode"
-//    case Artist = "artist"
 }
 
-public protocol SearchResult {
-    
-    var wrapperType: SearchResultWrapperType { get }
-    var kind: String { get }
-    var artistId: Int { get }
-    var collectionId: Int { get }
-    var trackId: Int { get }
-    var artistName: String { get }
-    var collectionName: String { get }
-    var trackName: String { get }
-    var collectionCensoredName: String { get }
-    var trackCensoredName: String { get }
-    
-    var artistViewUrl: String { get }
-    var collectionViewUrl: String { get }
-    var trackViewUrl: String { get }
+public enum SearchResultKind: String {
+    case Book = "book"
+    case Album = "album"
+    case CoachedAudio = "coached-audio"
+    case FeatureMovie = "feature-movie"
+    case InteractiveBooklet = "interactive-booklet"
+    case MusicVideo = "music-video"
+    case PdfPodcast = "pdf podcast"
+    case PodcastEpisode = "podcast-episode"
+    case SoftwarePackage = "software-package"
+    case Song = "song"
+    case TvEpisode = "tv-episode"
+    case Artist = "artist"
+
 }
 
-public struct SearchResultTrack: SearchResult {
+public enum SearchResult {
+    case Track(SearchResultTrack)
+    case Artist(SearchResultArtist)
+    case Collection(SearchResultCollection)
+}
+
+public struct SearchResultTrack {
     
-    public var wrapperType: SearchResultWrapperType
-    public var kind: String
-    public var artistId: Int
-    public var collectionId: Int
-    public var trackId: Int
-    public var artistName: String
-    public var collectionName: String
-    public var trackName: String
-    public var collectionCensoredName: String
-    public var trackCensoredName: String
+    public internal(set) var kind: SearchResultKind
+    public internal(set) var artistId: Int
+    public internal(set) var collectionId: Int
+    public internal(set) var trackId: Int
+    public internal(set) var artistName: String
+    public internal(set) var collectionName: String
+    public internal(set) var trackName: String
+    public internal(set) var collectionCensoredName: String
+    public internal(set) var trackCensoredName: String
     
     
-    public var artistViewUrl: String
-    public var collectionViewUrl: String
-    public var trackViewUrl: String
+    public internal(set) var artistViewUrl: String
+    public internal(set) var collectionViewUrl: String
+    public internal(set) var trackViewUrl: String
     
-    public var previewUrl: String
+    public internal(set) var previewUrl: String
     
-    public var artworkUrl30: String
-    public var artworkUrl60: String
-    public var artworkUrl100: String
+    public internal(set) var artworkUrl30: String
+    public internal(set) var artworkUrl60: String
+    public internal(set) var artworkUrl100: String
     
-    public var collectionPrice: Float
-    public var trackPrice: Float
+    public internal(set) var collectionPrice: Float
+    public internal(set) var trackPrice: Float
     
-    public var releaseDate: String
+    public internal(set) var releaseDate: String
     
-    public var collectionExplicitness: String
-    public var trackExplicitness: String
+    public internal(set) var collectionExplicitness: String
+    public internal(set) var trackExplicitness: String
     
-    public var discCount: Int
-    public var discNumber: Int
+    public internal(set) var discCount: Int
+    public internal(set) var discNumber: Int
     
-    public var trackCount: Int
-    public var trackNumber: Int
+    public internal(set) var trackCount: Int
+    public internal(set) var trackNumber: Int
     
-    public var trackTimeMillis: Int
+    public internal(set) var trackTimeMillis: Int
     
-    public var country: String
-    public var currency: String
+    public internal(set) var country: String
+    public internal(set) var currency: String
     
-    public var primaryGenreName: String
+    public internal(set) var primaryGenreName: String
     
-    public var radioStationUrl: String?
+    public internal(set) var radioStationUrl: String?
     
-    public var isStreamable: Bool
+    public internal(set) var isStreamable: Bool
+}
+
+public struct SearchResultArtist {
+    
+    public internal(set) var artistType: String
+    
+    public internal(set) var artistName: String
+    public internal(set) var artistLinkUrl: String
+    public internal(set) var artistId: Int
+    public internal(set) var amgArtistId: Int
+    
+    public internal(set) var primaryGenreName: String
+    public internal(set) var primaryGenreId: Int
+    
+    public internal(set) var radioStationUrl: String?
+}
+
+
+public struct SearchResultCollection {
+    
+    public internal(set) var collectionType: String
+    public internal(set) var artistId: Int
+    public internal(set) var collectionId: Int
+    public internal(set) var amgArtistId: Int
+    public internal(set) var artistName: String
+    public internal(set) var collectionName: String
+    public internal(set) var collectionCensoredName: String
+    
+    
+    public internal(set) var artistViewUrl: String
+    public internal(set) var collectionViewUrl: String
+    
+    
+    public internal(set) var artworkUrl30: String
+    public internal(set) var artworkUrl60: String
+    public internal(set) var artworkUrl100: String
+    
+    public internal(set) var collectionPrice: Float
+    
+    public internal(set) var collectionExplicitness: String
+    
+    public internal(set) var trackCount: Int
+    
+    public internal(set) var copyright: String
+    
+    public internal(set) var country: String
+    public internal(set) var currency: String
+    
+    public internal(set) var releaseDate: String
+    
+    public internal(set) var primaryGenreName: String
+    
+    public internal(set) var radioStationUrl: String?
 }
